b79adc1edd95a7a55b9fce3b0910f28add63d645
Ryan Phillips <ryanmphill@gmail.com>
2024-08-06 17:04:15 -0500

    Added client side routing for application

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,34 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
+import Home from './components/Home.jsx';
+import { Authorized } from './components/auth/Authorized.jsx';
+import Login from './components/auth/Login.jsx';
+import Register from './components/auth/Register.jsx';
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+      <>
+          <Route element={<App />} 
+              path="/"
+          >
+            <Route element={<Authorized />}>
+              <Route element={<Home />} path="/" />
+            </Route>
+            <Route element={<Login />} path="/login" />
+            <Route element={<Register />} path="/register" />
+          </Route>
+      </>
+  ));
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
